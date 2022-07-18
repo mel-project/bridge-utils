6645805f4f0bef35438cc15fb07040992d519438
sadministrator <marcothewebdev@gmail.com>
2022-07-18 14:20:33 -0500

    Better random struct functions.

--- app/src/main.rs
+++ app/src/main.rs
@@ -5,15 +5,37 @@ use std::sync::Arc;
 
 use bindings::themelio_bridge::ThemelioBridge;
 use blake3;
-use ed25519_compact::{KeyPair, Signature, Seed, Noise};
-use ethers::prelude::{Http, LocalWallet, Middleware, SignerMiddleware};
+use clap::Parser;
+use ed25519_compact::{
+    KeyPair,
+    Signature,
+    Seed, 
+    Noise
+};
+use ethers::prelude::{
+    Http,
+    LocalWallet, 
+    Middleware,
+    SignerMiddleware
+};
 use ethers::providers::Provider;
 use ethers::signers::Signer;
-use ethers::types::{Address as EthersAddress, Bytes, U256};
+use ethers::types::{
+    Address as EthersAddress,
+    Bytes,
+    U256
+};
 use eyre::Result;
 use rand::Rng;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use rs_merkle::{MerkleTree, MerkleProof, Hasher};
+use rayon::iter::{
+    IntoParallelIterator,
+    ParallelIterator
+};
+use rs_merkle::{
+    MerkleTree,
+    MerkleProof,
+    Hasher
+};
 use themelio_structs::{
     Address as ThemelioAddress,
     BlockHeight,
@@ -33,17 +55,28 @@ const DATA_BLOCK_HASH_KEY: &[u8; 13] = b"smt_datablock";
 const NODE_HASH_KEY: &[u8; 8] = b"smt_node";
 const GAS_LIMIT: u32 = 29_000_000;
 
-pub struct Config {
+struct Config {
     pub wallet: LocalWallet,
 }
 
 impl Config {
     pub fn new() -> Result<Config, &'static str> {
-        let secret_key = env::var("SECRET_KEY").expect("Missing SECRET_KEY env variable.");
-        let wallet: LocalWallet = secret_key.parse().unwrap();
+        let secret_key = env::var("SECRET_KEY")
+            .expect("Missing SECRET_KEY env variable.");
+
+        let wallet: LocalWallet = secret_key
+            .parse()
+            .unwrap();
 
         Ok(Config { wallet })
     }
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, default_value = "")]
+    data: String,
 }
 
 #[derive(Clone)]
@@ -57,42 +90,184 @@ impl Hasher for Blake3Algorithm {
     }
 }
 
-fn random_transaction() -> Transaction {
-    let inputs: Vec<CoinID> = vec![ CoinID {
+fn random_header(modifier: u128) -> Header {
+    if modifier == 0 {
+        Header {
+            network: NetID::Mainnet,
+            previous: HashVal::random(),
+            height: BlockHeight(u64::MIN),
+            history_hash: HashVal::random(),
+            coins_hash: HashVal::random(),
+            transactions_hash: HashVal::random(),
+            fee_pool: CoinValue(u128::MIN),
+            fee_multiplier: u128::MIN,
+            dosc_speed: u128::MIN,
+            pools_hash: HashVal::random(),
+            stakes_hash: HashVal::random(),
+        }
+    } else if modifier == u8::MAX.into() {
+        Header {
+            network: NetID::Mainnet,
+            previous: HashVal::random(),
+            height: BlockHeight(u8::MAX.into()),
+            history_hash: HashVal::random(),
+            coins_hash: HashVal::random(),
+            transactions_hash: HashVal::random(),
+            fee_pool: CoinValue(u8::MAX.into()),
+            fee_multiplier: u8::MAX.into(),
+            dosc_speed: u8::MAX.into(),
+            pools_hash: HashVal::random(),
+            stakes_hash: HashVal::random(),
+        }
+    } else if modifier == u16::MAX.into() {
+        Header {
+            network: NetID::Mainnet,
+            previous: HashVal::random(),
+            height: BlockHeight(u16::MAX.into()),
+            history_hash: HashVal::random(),
+            coins_hash: HashVal::random(),
+            transactions_hash: HashVal::random(),
+            fee_pool: CoinValue(u16::MAX.into()),
+            fee_multiplier: u16::MAX.into(),
+            dosc_speed: u16::MAX.into(),
+            pools_hash: HashVal::random(),
+            stakes_hash: HashVal::random(),
+        }
+    } else if modifier == u32::MAX.into() {
+        Header {
+            network: NetID::Mainnet,
+            previous: HashVal::random(),
+            height: BlockHeight(u32::MAX.into()),
+            history_hash: HashVal::random(),
+            coins_hash: HashVal::random(),
+            transactions_hash: HashVal::random(),
+            fee_pool: CoinValue(u32::MAX.into()),
+            fee_multiplier: u32::MAX.into(),
+            dosc_speed: u32::MAX.into(),
+            pools_hash: HashVal::random(),
+            stakes_hash: HashVal::random(),
+        }
+    } else if modifier == u64::MAX.into() {
+        Header {
+            network: NetID::Mainnet,
+            previous: HashVal::random(),
+            height: BlockHeight(u64::MAX),
+            history_hash: HashVal::random(),
+            coins_hash: HashVal::random(),
+            transactions_hash: HashVal::random(),
+            fee_pool: CoinValue(u64::MAX.into()),
+            fee_multiplier: u64::MAX.into(),
+            dosc_speed: u64::MAX.into(),
+            pools_hash: HashVal::random(),
+            stakes_hash: HashVal::random(),
+        }
+    } else if modifier == u128::MAX {
+        Header {
+            network: NetID::Mainnet,
+            previous: HashVal::random(),
+            height: BlockHeight(u64::MAX),
+            history_hash: HashVal::random(),
+            coins_hash: HashVal::random(),
+            transactions_hash: HashVal::random(),
+            fee_pool: CoinValue(u128::MAX),
+            fee_multiplier: u128::MAX,
+            dosc_speed: u128::MAX,
+            pools_hash: HashVal::random(),
+            stakes_hash: HashVal::random(),
+        }
+    } else {
+        Header {
+            network: NetID::Mainnet,
+            previous: HashVal::random(),
+            height: BlockHeight(rand::thread_rng().gen()),
+            history_hash: HashVal::random(),
+            coins_hash: HashVal::random(),
+            transactions_hash: HashVal::random(),
+            fee_pool: CoinValue(rand::thread_rng().gen()),
+            fee_multiplier: rand::thread_rng().gen(),
+            dosc_speed: rand::thread_rng().gen(),
+            pools_hash: HashVal::random(),
+            stakes_hash: HashVal::random(),
+        }
+    }
+}
+
+fn random_coin_id() -> CoinID {
+    CoinID {
         txhash: TxHash(HashVal::random()),
         index: rand::thread_rng().gen(),
-    }];
+    }
+}
 
-    //let range: Range<u32> = 0..20;
-    //
-    // let recipient_address = range.into_par_iter().map(|_index| {
-    //     rand::thread_rng().gen()
-    // }).collect::<Vec<u8>>();
-    let recipient_address: Vec<u8> = Config::new()
-        .expect("Missing env variables.")
-        .wallet.address()
-        .as_bytes()
-        .to_vec();
+fn random_coindata() -> CoinData {
+    let additional_data_size: u32 = rand::thread_rng().gen_range(0..32);
+    let additional_data_range = 0..additional_data_size;
+    let additional_data: Vec<u8> = additional_data_range
+        .map(|_| {
+            rand::thread_rng().gen::<u8>()
+        })
+        .collect();
 
-    let output_one: CoinData = CoinData {
+    CoinData {
         covhash: ThemelioAddress(HashVal::random()),
         value: CoinValue(rand::thread_rng().gen()),
         denom: Denom::Mel,
-        additional_data: recipient_address,
-    };
+        additional_data
+    }
+}
 
-    let output_two: CoinData = CoinData {
-        covhash: ThemelioAddress(HashVal::random()),
-        value: CoinValue(rand::thread_rng().gen()),
-        denom: Denom::Mel,
-        additional_data: vec![]
-    };
+fn random_transaction() -> Transaction {
+    let limit: u32 = 32;
 
-    let outputs: Vec<CoinData> = vec![output_one, output_two];
+    let num_inputs: u32 = rand::thread_rng().gen_range(1..limit);
+    let inputs = (0..num_inputs)
+        .into_iter()
+        .map(|_| {
+            random_coin_id()
+        })
+        .collect();
 
-    let covenants: Vec<Vec<u8>> = vec![vec![rand::thread_rng().gen()]];
+    let num_outputs: u32 = rand::thread_rng().gen_range(1..limit);
+    let outputs = (0..num_outputs)
+        .into_iter()
+        .map(|_| {
+            random_coindata()
+        })
+        .collect();
 
-    let sigs: Vec<Vec<u8>> = vec![vec![rand::thread_rng().gen()]];
+    let num_covenants: u32 = rand::thread_rng().gen_range(1..limit);
+    let covenants = (0..num_covenants)
+        .into_iter()
+        .map(|_| {
+            let size = rand::thread_rng().gen_range(0..limit);
+            let range = 0..size;
+            let covenant = range
+                .into_iter()
+                .map(|_| {
+                    rand::thread_rng().gen::<u8>()
+                })
+                .collect();
+
+            covenant
+        })
+        .collect();
+
+    let num_sigs: u32 = rand::thread_rng().gen_range(1..limit);
+    let sigs = (0..num_sigs)
+        .into_iter()
+        .map(|_| {
+            let size = rand::thread_rng().gen_range(0..limit);
+            let range = 0..size;
+            let sig = range
+                .into_iter()
+                .map(|_| {
+                    rand::thread_rng().gen::<u8>()
+                })
+                .collect();
+
+            sig
+        })
+        .collect();
 
     Transaction {
         kind: TxKind::Swap,
@@ -111,22 +286,6 @@ fn create_datablocks(num: u32) -> Vec<Transaction> {
     range.into_par_iter().map(|_index| {
         random_transaction()
     }).collect::<Vec<Transaction>>()
-}
-
-fn random_header() -> Header {
-    Header {
-        network: NetID::Mainnet,
-        previous: HashVal::random(),
-        height: BlockHeight(rand::thread_rng().gen()),
-        history_hash: HashVal::random(),
-        coins_hash: HashVal::random(),
-        transactions_hash: HashVal::random(),
-        fee_pool: CoinValue(rand::thread_rng().gen()),
-        fee_multiplier: rand::thread_rng().gen(),
-        dosc_speed: rand::thread_rng().gen(),
-        pools_hash: HashVal::random(),
-        stakes_hash: HashVal::random(),
-    }
 }
 
 fn random_staker(message: &[u8]) -> ([u8; 32], U256, [u8; 64]) {
@@ -163,7 +322,7 @@ fn create_stakers(num: u32, message: &[u8]) -> (Vec<[u8; 32]>, Vec<U256>, Vec<[u
 }
 
 // TESTS
-async fn setup_bridge(address: &str) -> ThemelioBridge<SignerMiddleware<Provider<Http>, LocalWallet>>{
+async fn setup_bridge(address: &str) -> ThemelioBridge<SignerMiddleware<Provider<Http>, LocalWallet>> {
     let provider = Provider::<Http>::try_from(
         "https://rinkeby.infura.io/v3/0771c92c5c6c42669665a80daa68b848",
     ).expect("Provider unable to be initialized.");
@@ -191,7 +350,8 @@ async fn test_e2e(address: &str, num_stakers: u32, merkle_tree_height: u32) -> R
     let bridge = setup_bridge(address).await;
 
     // create a random block header
-    let mut header: Header = random_header();
+    let modifier: u128 = rand::thread_rng().gen();
+    let mut header: Header = random_header(modifier);
 
     let block_height = U256::from(header.height.0);
 
@@ -257,7 +417,6 @@ async fn test_e2e(address: &str, num_stakers: u32, merkle_tree_height: u32) -> R
 
     println!("\n***** relayHeader() receipt *****\n{:#?}\n********************\n", receipt.unwrap());
     // assert
-
     // send tx to verifyTx()
     let call = bridge
         .verify_tx(serialized_tx_bytes.clone(), index_u256, block_height, merkle_proof_vec.clone())
@@ -280,31 +439,42 @@ async fn test_e2e(address: &str, num_stakers: u32, merkle_tree_height: u32) -> R
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let address = "0x8e27C1C496dD6D850E62e0825eD120e1b6d0b560";
+async fn main() {//} -> Result<()> {
+    let args = Args::parse();
 
-    let mut num_stakers = String::new();
-    let mut merkle_tree_height = String::new();
+    if args.data.len() > 0 {
+        println!("{}", args.data);
+    } else {
+        let address = "0x8e27C1C496dD6D850E62e0825eD120e1b6d0b560";
 
-    println!("Input the desired number of stakers: ");
-    io::stdin()
-        .read_line(&mut num_stakers)
-        .expect("Failed to read number of stakers.");
+        let mut num_stakers = String::new();
+        let mut merkle_tree_height = String::new();
 
-    println!("Input the desired Merkle tree height: ");
-    io::stdin()
-        .read_line(&mut merkle_tree_height)
-        .expect("Failed to read Merkle tree height.");
+        println!("Input the desired number of stakers: ");
+        io::stdin()
+            .read_line(&mut num_stakers)
+            .expect("Failed to read number of stakers.");
 
-    let num_stakers: u32 = num_stakers
-        .trim()
-        .parse()?;
+        println!("Input the desired Merkle tree height: ");
+        io::stdin()
+            .read_line(&mut merkle_tree_height)
+            .expect("Failed to read Merkle tree height.");
 
-    let merkle_tree_height: u32 = merkle_tree_height
-        .trim()
-        .parse()?;
+        let num_stakers: u32 = num_stakers
+            .trim()
+            .parse()
+            .expect("Error parsing number of stakers.");
 
-    test_e2e(address, num_stakers, merkle_tree_height).await?;
+        let merkle_tree_height: u32 = merkle_tree_height
+            .trim()
+            .parse()
+            .expect("Error parsing Merkle tree height.");
 
-    Ok(())
+        test_e2e(address, num_stakers, merkle_tree_height)
+            .await
+            .expect("Error awaiting end-to-end test.");
+    }
+    
+    print!("{}", blake3_differential());
+    //Ok(())
 }
